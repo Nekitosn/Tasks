7f0db6110f2f5151c62ad8c4b27bc4c0f6801ad4
Nekitosn <nikitasedov57@gmail.com>
2021-11-19 18:13:40 +0200

    some add

---
+++ Box/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Box
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            Fighter sanya = new Fighter("Sanya", 228, 69);
+            Fighter oleg = new Fighter("Oleg", 322, 33);
+
+            Fighter.Fight(sanya, oleg, rand.Next(0, 2));
+        }
+    }
+}
